1aa083206e6f9894ec09e73722db725b9e63244f
Denis Faivre <x-github@ablivio.com>
2014-12-21 12:48:38 +0100

    Re-indented to comply with recommendation of minimal 4-char indents (although I find 2-char is just fine).

--- cachematrix.R
+++ cachematrix.R
@@ -18,33 +18,33 @@
 ## - setcache(c) / getcache(): getter & setter of the cache
 
 makeCacheMatrix <- function(value = matrix()) {
-  
-  ## cache is initially unset
-  cache <- NULL
-  
-  ## value setter: records new value and resets cache
-  set <- function(m) {
-    value <<- m
-    cache <<- NULL
-  }
-  
-  ## value getter
-  get <- function() value
-  
-  ## cache setter
-  setcache <- function(c) {
-    cache <<- c
-  }
-  
-  ## cache getter
-  getcache <- function() cache
-
-  list(
-    set = set,
-    get = get,
-    setcache = setcache,
-    getcache = getcache
-  )
+	
+	## cache is initially unset
+	cache <- NULL
+	
+	## value setter: records new value and resets cache
+	set <- function(m) {
+		value <<- m
+		cache <<- NULL
+	}
+	
+	## value getter
+	get <- function() value
+	
+	## cache setter
+	setcache <- function(c) {
+		cache <<- c
+	}
+	
+	## cache getter
+	getcache <- function() cache
+	
+	list(
+		set = set,
+		get = get,
+		setcache = setcache,
+		getcache = getcache
+	)
 }
 
 
@@ -57,18 +57,19 @@ makeCacheMatrix <- function(value = matrix()) {
 ## Otherwise the inverse is computed and stored in the cache.
 
 cacheSolve <- function(mat, ...) {
-  ## if cache is set we don't need to compute the inverse
-  cache <- mat$getcache()
-  if (!is.null(cache)) {
-    message("getting cached data")
-    inverse <- cache
-  }
-  else {
-    ## cache is not yet set -- let's compute the inverse
-    value <- mat$get()
-    inverse <- solve(value)
-    mat$setcache(inverse)
-  }
-  inverse
+	
+	## if cache is set we don't need to compute the inverse
+	cache <- mat$getcache()
+	if (!is.null(cache)) {
+		message("getting cached data")
+		inverse <- cache
+	}
+	else {
+		## cache is not yet set -- let's compute the inverse
+		value <- mat$get()
+		inverse <- solve(value)
+		mat$setcache(inverse)
+	}
+	inverse
 }
 
